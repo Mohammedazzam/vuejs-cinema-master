2a6bb41a79ad0080307e3a100f6a5036c4e14ca9
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-01-30 23:19:20 +0200

    (67) Setup the check-filter component

--- src/main.js
+++ src/main.js
@@ -1,27 +1,43 @@
-import Vue from 'vue';
-import './style.scss';
+import Vue from "vue";
+import "./style.scss";
+
+import genres from './util/genres';
 
 new Vue({
-  el: '#app',
+  el: "#app",
   components: {
-    'movie-list': {
+    "movie-list": {
       template: `<div id="movie-list">
                     <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
                 </div>`,
       data() {
         return {
           movies: [
-            { title: 'Pulp Fiction' },
-            { title: 'Home Alone' },
-            { title: 'Austin Powers' }
+            { title: "Pulp Fiction" },
+            { title: "Home Alone" },
+            { title: "Austin Powers" }
           ]
-        }
+        };
       }
     },
-    'movie-filter': {
+    "movie-filter": {
+        data(){
+            return {
+                genres
+            }
+        },
       template: `<div id="movie-filter">
                     <h2>Filter results</h2>
-                </div>`
+                    <div class="filter-group">
+                        <check-filters v-for="genre in genres"></check-filters>
+                    </div>
+                </div>`,
+
+      components: {
+        "check-filters": {
+          template:`<div>Filter</div>`
+        }
+      }
     }
   }
 });

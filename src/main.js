49915d65d51dfb139cf74a589a9fc1b8030bd7ba
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-02 00:21:04 +0200

    (75) Processing check-filter event in root instance

--- src/main.js
+++ src/main.js
@@ -1,70 +1,79 @@
-import Vue from "vue";
-import "./style.scss";
+import Vue from 'vue';
+import './style.scss';
 
-import genres from "./util/genres";
+import genres from './util/genres';
 
 new Vue({
-    el: "#app",
-    methods: {
-        checkFilter(category, title, checked){
-            console.log(category, title, checked)
+  el: '#app',
+  data: {
+    genre: [],
+    time: []
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
         }
-    },
-    components: {
-        "movie-list": {
-            template: `<div id="movie-list">
+      }
+    }
+  },
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
                     <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
                 </div>`,
-            data() {
-                return {
-                    movies: [
-                        { title: "Pulp Fiction" },
-                        { title: "Home Alone" },
-                        { title: "Austin Powers" }
-                    ]
-                };
-            }
-        },
-        "movie-filter": {
-            data() {
-                return {
-                    genres
-                };
-            },
-            template: `<div id="movie-filter">
+      data() {
+        return {
+          movies: [
+            { title: 'Pulp Fiction', genre: genres.CRIME },
+            { title: 'Home Alone', genre: genres.COMEDY },
+            { title: 'Austin Powers', genre: genres.COMEDY }
+          ]
+        }
+      },
+      props: [ 'genre', 'time' ]
+    },
+    'movie-filter': {
+      data() {
+        return {
+          genres
+        };
+      },
+      template: `<div id="movie-filter">
                     <h2>Filter results</h2>
                     <div class="filter-group">
-                        <check-filters v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filters>
-                    </div>
+                      <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>                   
+                    </div>                 
                 </div>`,
-
-                methods: {
-                    checkFilter(category, title, checked){
-                        this.$emit('check-filter', category, title, checked)
-                    }
-                },
-
-
-            components: {
-                "check-filters": {
-                    data(){
-                        return{
-                            checked: false 
-                        }
-                    },
-                    props: ["title"],
-                    template: `<div v-bind:class="{'check-filter':true, active: checked}" v-on:click="checkFilter">
+      methods: {
+        checkFilter(category, title, checked) {
+          this.$emit('check-filter', category, title, checked);
+        }
+      },
+      components: {
+        'check-filter': {
+          data() {
+            return {
+              checked: false
+            }
+          },
+          props: [ 'title' ],
+          template: `<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checkFilter">
                         <span class="checkbox"></span>
-                        <span class="check-filter-title">{{title}}</span>
+                        <span class="check-filter-title">{{ title }}</span>
                     </div>`,
           methods: {
-              checkFilter(){
-                  this.checked = !this.checked;
-                  this.$emit('check-filte','genre', this.title, this.checked);
-              }
-          },
-                }
+            checkFilter() {
+              this.checked = !this.checked;
+              this.$emit('check-filter', 'genre', this.title, this.checked);
             }
+          }
         }
+      }
     }
+  }
 });
